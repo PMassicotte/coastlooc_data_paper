e894bcbaa49dd85047bdabb40fd437f7fc119faf
Philippe Massicotte <pmassicotte@hotmail.com>
2021-03-16 16:30:26 -0400

    Do not plot Massimo's data.

--- R/05_extract_ferrari_2000_extra_data.R
+++ R/05_extract_ferrari_2000_extra_data.R
@@ -49,43 +49,6 @@ massimo <- bind_rows(df1, df2) %>%
 
 massimo
 
-# Compare scdom between Ferrari and Marcel data ---------------------------
-
-scdom_ferrari <- massimo %>%
-  mutate(station = str_replace(station, "VH", "C1")) %>%
-  mutate(station = str_replace(station, "The", "C4")) %>%
-  extract(station, into = c("cruise", "station"), regex = "(.*)(\\d{2})$")
-
-scdom_ferrari
-
-scdom_babin <- read_csv("data/clean/surface.csv") %>%
-  select(station, s_cdom_model) %>%
-  filter(str_detect(station, "^C1|^C4")) %>%
-  extract(station, into = c("cruise", "station"), regex = "(.{2}).(.{2})")
-
-df_viz <- scdom_ferrari %>%
-  left_join(scdom_babin) %>%
-  mutate(cruise = case_when(
-    cruise == "C1" ~ "C1 (VH)",
-    cruise == "C4" ~ "C4 (The)",
-  ))
-
-p <- df_viz %>%
-  ggplot(aes(x = s_cdom, s_cdom_model, color = cruise)) +
-  geom_point() +
-  geom_abline(lty = 2, size = 0.25) +
-  ggrepel::geom_text_repel(aes(label = station), show.legend = FALSE, size = 2) +
-  labs(
-    x = quote(S[cdom] ~ (Marcel)),
-    y = quote(S[cdom] ~ (Ferrari))
-  )
-
-ggsave(
-  here("graphs/06_scdom_ferrari_vs_babin.pdf"),
-  device = cairo_pdf,
-  width = 6,
-  height = 4
-)
 
 # Export data -------------------------------------------------------------
 
