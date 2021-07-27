1beff0ee6c32690ae31e0dbffddc500eccf073fd
Philippe Massicotte <pmassicotte@hotmail.com>
2021-07-27 13:02:53 -0400

    Show measured acdom spectra instead of modelled ones.

--- R/fig04.R
+++ R/fig04.R
@@ -72,7 +72,7 @@ p3 <- df_viz %>%
   )
 
 p4 <- absorption %>%
-  ggplot(aes(x = wavelength, y = a_cdom_modeled, color = area)) +
+  ggplot(aes(x = wavelength, y = a_cdom_measured, color = area)) +
   geom_line() +
   scale_color_manual(
     breaks = area_breaks,
@@ -113,14 +113,54 @@ ggsave(
 
 # Stats for the paper -----------------------------------------------------
 
+## Range of sCDOM on the averaged spectra per area ----
+
+df_viz
+
+range(df_viz$wavelength, na.rm = TRUE)
+
+df_scdom <- df_viz %>%
+  select(area, wavelength, a_cdom_modeled) %>%
+  filter(between(wavelength, 350, 700)) %>%
+  group_nest(area) %>%
+  mutate(model = map(data, ~ nls(
+    a_cdom_modeled ~ a0 * exp(-s * (wavelength - 400)) + k,
+    data = .,
+    start = list(a0 = 0.5, s = 0.02, k = 0)
+  ))) %>%
+  mutate(tidied = map(model, broom::tidy))
+
+df_scdom
+
+df_scdom %>%
+  unnest(tidied)
+
+df_scdom %>%
+  mutate(pred = map(model, broom::augment)) %>%
+  unnest(pred) %>%
+  ggplot(aes(x = wavelength, y = a_cdom_modeled, color = area)) +
+  geom_point() +
+  geom_line(aes(y = .fitted)) +
+  scale_color_manual(
+    breaks = area_breaks,
+    values = area_colors
+  )
+
+df_scdom %>%
+  unnest(tidied) %>%
+  filter(term == "s") %>%
+  mutate(area = fct_reorder(area, estimate)) %>%
+  ggplot(aes(x = estimate, y = area)) +
+  geom_col()
+
 ## Range of acdom(350) ----
 
 absorption <- read_csv(here("data","clean","absorption.csv")) %>%
-  filter(wavelength == 443) %>%
+  filter(wavelength == 350) %>%
   inner_join(station)
 
 absorption %>%
-  summarise(across(starts_with("a_"), list(
+  summarise(across(starts_with("a_cdom"), list(
     min = ~ min(., na.rm = TRUE),
     max = ~ max(., na.rm = TRUE)
   ))) %>%
