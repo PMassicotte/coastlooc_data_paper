4b558cda9d02db0135e05a156734c8b939c5979f
Philippe Massicotte <pmassicotte@hotmail.com>
2022-12-09 08:23:37 -0500

    Show reflectance instead of irradiance in figure 6.

--- R/fig06.R
+++ R/fig06.R
@@ -11,49 +11,46 @@ source(here("R", "zzz.R"))
 stations <- read_csv(here("data", "clean", "stations.csv")) |>
   select(station, area)
 
-irradiance <- read_csv(here("data", "clean", "irradiance.csv"))
+reflectance <- read_csv(here("data", "clean", "reflectance.csv"))
 
-irradiance
+reflectance
 
-irradiance <- irradiance |>
-  drop_na(ed_w_m2_um) |>
+reflectance <- reflectance |>
+  drop_na(measured_reflectance_percent) |>
   inner_join(stations, by = "station")
 
 # There are NAs at some wavelengths due to the usage of different devices. For
 # visualization, I will remove mean values that were computed with a limited
 # number of observations.
 
-irradiance_mean <- irradiance |>
+irradiance_mean <- reflectance |>
   group_by(area, wavelength) |>
-  summarise(ed_w_m2_um = mean(ed_w_m2_um, na.rm = TRUE), n = n()) |>
+  summarise(measured_reflectance_percent = mean(measured_reflectance_percent, na.rm = TRUE), n = n()) |>
   ungroup() |>
   filter(n >= 10)
 
-irradiance |>
+reflectance |>
   ggplot(aes(
     x = wavelength,
-    y = ed_w_m2_um,
+    y = measured_reflectance_percent,
     color = area,
     group = station
   )) +
-  geom_line(size = 0.1, alpha = 0.5) +
+  geom_line(linewidth = 0.1, alpha = 0.5) +
   geom_line(
     data = irradiance_mean,
-    aes(x = wavelength, y = ed_w_m2_um, color = area),
-    size = 1.25,
+    aes(x = wavelength, y = measured_reflectance_percent, color = area),
+    linewidth = 1,
     inherit.aes = FALSE
   ) +
+  scale_y_continuous(labels = scales::label_percent()) +
   scale_color_manual(
     breaks = area_breaks,
     values = area_colors
   ) +
   labs(
     x = "Wavelength (mm)",
-    y = quote(E[d](0^"-") ~ "[" ~ W ~ m^{
-      -2
-    } ~ mu * m^{
-      -1
-    } ~ "]")
+    y = parse(text = "R(0^'-')")
   ) +
   facet_wrap(~area) +
   theme(legend.position = "none")
